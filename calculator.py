bbaeff9e604012318dc12664450631f14cad1e65
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 16:04:18 -0700

    Added to Calculator REPL the functionalities of *, /, square, cube, pow, and mod.

--- calculator.py
+++ calculator.py
@@ -30,5 +30,35 @@ def calculator_interface():
 			num2 = int(tokens[2])
 			total = subtract(num1, num2)
 			print total
+		elif tokens[0] == "*":
+			num1 = int(tokens[1])
+			num2 = int(tokens[2])
+			total = multiply(num1, num2)
+			print total
+		elif tokens[0] == "/":
+			num1 = int(tokens[1])
+			num2 = int(tokens[2])
+			total = divide(num1, num2)
+			print total
+		elif tokens[0] == "square":
+			num1 = int(tokens[1])
+			total = square(num1)
+			print total
+		elif tokens[0] == "cube":
+			num1 = int(tokens[1])
+			total = cube(num1)
+			print total
+		elif tokens[0] == "pow":
+			num1 = int(tokens[1])
+			num2 = int(tokens[2])
+			total = power(num1, num2)
+			print total
+		elif tokens[0] == "mod":
+			num1 = int(tokens[1])
+			num2 = int(tokens[2])
+			total = mod(num1, num2)
+			print total
+		else:
+			print "I do not understand that command. Please try again!"
 
 calculator_interface()
