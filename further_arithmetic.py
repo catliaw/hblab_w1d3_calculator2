13a993ba1c64ed4374e8f7ee25cf295a942b671f
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 18:00:34 -0700

    Further Arithmetic: added functions add, subtract and multiply to take multiple arguments.

--- further_arithmetic.py
+++ further_arithmetic.py
@@ -1,18 +1,49 @@
 def add(num_list):
-	"""Takes 2 integers and returns the sum of the inputs."""
+	"""Takes integers and returns the sum of the inputs."""
 	for ind, item in enumerate(num_list):
 		if ind != 0:
 			num_list[ind] = int(item)
-	num_list
-	return num1 + num2
+			int_num_list = num_list[1:]
+			total = sum(int_num_list)
+			print total
+			return total
+listy = ["+", 1, 2, 3, 4, 5]
+add(listy)
 
-# def subtract(num_list):
-# 	"""Takes 2 integers and returns the result of subtracting the second number from the first."""
-# 	return num1 - num2
+def subtract(num_list):
+	"""Takes integers and returns the result of subtracting in sequence from left to right."""
+	for ind, item in enumerate(num_list):
+		if ind != 0:
+			num_list[ind] = int(item)
+			int_num_list = num_list[1:]
+			total = None
+			for num in int_num_list:
+				if total == None:
+					total = num
+				else:
+					total = total - num
+			print total
+			return total
+listy = ["+", 10, 5, 4, 3, 1]
+subtract(listy)
 
-# def multiply(num_list):
-# 	"""Takes 2 integers and returns the product of the inputs."""
-# 	return num1 * num2
+
+def multiply(num_list):
+	"""Takes integers and returns the product of the inputs."""
+	for ind, item in enumerate(num_list):
+		if ind != 0:
+			num_list[ind] = int(item)
+			int_num_list = num_list[1:]
+			total = None
+			for num in int_num_list:
+				if total == None:
+					total = num
+				else:
+					total = total * num
+			print total
+			return total
+listy = ["*", 10, 5, 4, 3, 1]
+multiply(listy)
 
 # def divide(num_list):
 # 	"""Takes 2 integers, divides the first input by the second, returning a floating point number."""
